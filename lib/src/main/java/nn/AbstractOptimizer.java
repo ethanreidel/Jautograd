8a937a8e1fd21bceecf9ec7b4f15650f731d6ea8
Eric Yamabe <ericyamabe@gmail.com>
2025-12-11 18:36:16 -0800

    adding atom adam and rms prop optimizer

--- lib/src/main/java/nn/AbstractOptimizer.java
+++ lib/src/main/java/nn/AbstractOptimizer.java
@@ -4,13 +4,17 @@ import java.util.List;
 import scalar.Scalar;
 
 public abstract class AbstractOptimizer implements Optimizer{
-    protected final double learningRate;
+    protected double learningRate;
 
     protected AbstractOptimizer(double learningRate) {
         this.learningRate = learningRate;
     }
 
+    public void setLearningRate(double lr) {
+        this.learningRate = lr;
+    }
+
     @Override
     public abstract void step(List<Scalar> parameters);
-    
+
 }
